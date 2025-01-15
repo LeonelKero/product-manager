937760b7d09d1af9603ecce8980679bbf53d3402
leonel ka <waboleonel@gmail.com>
2025-01-15 19:20:30 +0100

    Define product model class

---
+++ src/main/java/com/alshadowstechnologies/productmanager/product/Product.java
@@ -0,0 +1,33 @@
+package com.alshadowstechnologies.productmanager.product;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    public Product(String name, String description, Double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+}
