ffcbc5c01a25428e56b3259d72b3d95515d4017b
leonel ka <waboleonel@gmail.com>
2025-01-15 20:11:10 +0100

    Create product repository

---
+++ src/main/java/com/alshadowstechnologies/productmanager/product/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.alshadowstechnologies.productmanager.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findProductByPrice(final Double price);
+
+}
