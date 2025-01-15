0dc7b1344f3cc99a573e3e6b67c6f9bef887b4dc
leonel ka <waboleonel@gmail.com>
2025-01-15 18:18:20 +0100

    Initial commit

---
+++ src/test/java/com/alshadowstechnologies/productmanager/ProductManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alshadowstechnologies.productmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
