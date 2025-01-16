3ffac84e53c84359eb29bd9d8aaf14e50cd02137
leonel ka <waboleonel@gmail.com>
2025-01-16 12:34:52 +0100

    Code refactoring

--- src/main/java/com/alshadowstechnologies/productmanager/product/dto/ProductRequestDto.java
+++ src/main/java/com/alshadowstechnologies/productmanager/product/dto/ProductRequestDto.java
@@ -1,4 +1,8 @@
 package com.alshadowstechnologies.productmanager.product.dto;
 
-public record ProductRequestDto(String name, String description, Double price, String categoryName) {
+public record ProductRequestDto(
+        String name,
+        String description,
+        Double price,
+        String categoryName) {
 }
