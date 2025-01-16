36f760fdee0a0ebec491ca6c97c508f5830d07d7
leonel ka <waboleonel@gmail.com>
2025-01-16 19:01:24 +0100

    Configure user details

--- src/main/java/com/alshadowstechnologies/productmanager/config/security/SecurityConfiguration.java
+++ src/main/java/com/alshadowstechnologies/productmanager/config/security/SecurityConfiguration.java
@@ -2,31 +2,31 @@ package com.alshadowstechnologies.productmanager.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfiguration(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsService() {
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin);
+    public void configure(final AuthenticationManagerBuilder builder) throws Exception {
+        builder
+                .userDetailsService(this.userDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
     }
 
 }
