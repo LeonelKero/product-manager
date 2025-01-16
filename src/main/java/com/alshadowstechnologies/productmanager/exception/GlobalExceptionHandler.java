2c5d0f515e03ab51c28bda88a6f7ea2ff3364266
leonel ka <waboleonel@gmail.com>
2025-01-16 16:54:34 +0100

    Code refactoring, switch to rest controller advice

--- src/main/java/com/alshadowstechnologies/productmanager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/alshadowstechnologies/productmanager/exception/GlobalExceptionHandler.java
@@ -3,10 +3,10 @@ package com.alshadowstechnologies.productmanager.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = {ProductNotFoundException.class})
@@ -33,7 +33,8 @@ public class GlobalExceptionHandler {
                 .getFieldErrors()
                 .stream()
                 .map(error -> error.getField() + ": " + error.getDefaultMessage())
-                .toList();;
+                .toList();
+        ;
         final var apiError = new ApiErrorResponse(HttpStatus.BAD_REQUEST.value(), "", String.join(", ", errors));
         return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
     }
