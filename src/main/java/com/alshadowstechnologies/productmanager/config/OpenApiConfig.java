c34b5d91fc3e6bde67d1fa0dce7525026a5afcb1
leonel ka <waboleonel@gmail.com>
2025-01-16 16:53:58 +0100

    Configure application open api

---
+++ src/main/java/com/alshadowstechnologies/productmanager/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.alshadowstechnologies.productmanager.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI productManagerOpenApi() {
+        return new OpenAPI().info(new Info()
+                .title("Product Manager REST API")
+                .version("1.0")
+                .description("Alshadows Technology Product Manager REST API")
+        );
+    }
+
+}
