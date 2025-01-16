9866f0eb56cf69f65cea81b6e28c0888193ddefd
leonel ka <waboleonel@gmail.com>
2025-01-16 01:05:19 +0100

    Add relationship with category model, and implement a constructor and toString method

--- src/main/java/com/alshadowstechnologies/productmanager/product/Product.java
+++ src/main/java/com/alshadowstechnologies/productmanager/product/Product.java
@@ -1,5 +1,6 @@
 package com.alshadowstechnologies.productmanager.product;
 
+import com.alshadowstechnologies.productmanager.category.Category;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,9 +26,29 @@ public class Product {
     @Column(nullable = false)
     private Double price;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Category category;
+
     public Product(String name, String description, Double price) {
         this.name = name;
         this.description = description;
         this.price = price;
+    }
+
+    public Product(String name, String description, Double price, Category category) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
     }
 }
