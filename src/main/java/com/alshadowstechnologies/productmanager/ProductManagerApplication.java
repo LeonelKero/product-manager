daba6ff5d3b032bca13e1a8b1c8c6737ff75c388
leonel ka <waboleonel@gmail.com>
2025-01-16 19:12:59 +0100

    Implement custom basic authentication

--- src/main/java/com/alshadowstechnologies/productmanager/ProductManagerApplication.java
+++ src/main/java/com/alshadowstechnologies/productmanager/ProductManagerApplication.java
@@ -4,7 +4,8 @@ import com.alshadowstechnologies.productmanager.category.Category;
 import com.alshadowstechnologies.productmanager.category.CategoryRepository;
 import com.alshadowstechnologies.productmanager.product.Product;
 import com.alshadowstechnologies.productmanager.product.ProductRepository;
-import org.apache.commons.lang3.StringUtils;
+import com.alshadowstechnologies.productmanager.user.AppUser;
+import com.alshadowstechnologies.productmanager.user.AppUserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,14 +15,22 @@ import java.util.Arrays;
 
 @SpringBootApplication
 public class ProductManagerApplication {
+    private final CategoryRepository categoryRepository;
+    private final ProductRepository productRepository;
+    private final AppUserRepository userRepository;
+
+    public ProductManagerApplication(CategoryRepository categoryRepository, ProductRepository productRepository, AppUserRepository userRepository) {
+        this.categoryRepository = categoryRepository;
+        this.productRepository = productRepository;
+        this.userRepository = userRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProductManagerApplication.class, args);
     }
 
     @Bean
-    CommandLineRunner run(final CategoryRepository categoryRepository,
-                          final ProductRepository productRepository, String... args) throws Exception {
+    CommandLineRunner run(String... args) throws Exception {
         return args1 -> {
 
             final var laptop = categoryRepository.save(new Category("Laptop"));
@@ -31,6 +40,11 @@ public class ProductManagerApplication {
             final var training = new Product("Nike Training", "Comfortable shoes for walking", 95.50, shoes);
 
             productRepository.saveAll(Arrays.asList(dell, training));
+
+            // ***--1234567 -> $2a$10$2AksZww/JXoc2JSiU7EvJOqXosFdocLnQrE0poevTzeR4544xGvf6 - B
+            // ---***1234567 -> $2a$10$zGMhaozvxy2pSCHa1DkxJuj.SQ3XokGDJLkOWoohhRTBq5r4fG/am - R
+            userRepository.save(new AppUser("workbeattalent@gmail.com", "$2a$10$2AksZww/JXoc2JSiU7EvJOqXosFdocLnQrE0poevTzeR4544xGvf6", "ADMIN"));
+            userRepository.save(new AppUser("waboleonel@gmail.com", "$2a$10$zGMhaozvxy2pSCHa1DkxJuj.SQ3XokGDJLkOWoohhRTBq5r4fG/am", "USER"));
 
             for (Product product : productRepository.findAll()) {
                 System.out.println(product);
